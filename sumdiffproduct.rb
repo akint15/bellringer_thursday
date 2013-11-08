cd5aa418d01d9c7ccb1342664ad8bd9c192c4176
akint15 <akint@chicagotechacademy.org>
2013-11-08 13:21:16 -0600

    change to sumdiffproduct.rb

--- sumdiffproduct.rb
+++ sumdiffproduct.rb
@@ -1,8 +1,15 @@
-puts "Enter 2 numbers"
+def  method_name(car, truck)
+	
+	puts "Enter 2 numbers"
 
-car = gets.chomp.to_i
-truck = gets.chomp.to_i
+	car = gets.split.to_i
+	truck = gets.split.to_i
 
-puts car+truck
-puts car-truck
-puts car*truck
+	puts car+truck
+	puts car-truck
+	puts car*truck
+	
+end
+
+
+method_name(1,3)
