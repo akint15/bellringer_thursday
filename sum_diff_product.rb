4c17415b2064188fe8f87bd49f786b5af9ed91b8
akint15 <akint@chicagotechacademy.org>
2013-11-08 13:05:16 -0600

    sumdiffproduct added

---
+++ sum_diff_product.rb
@@ -0,0 +1,15 @@
+ </head>
+ <body>
+ <div id="header" ><p>header</p></div>
+<div id="navbar"><p>nav</p></div>
+<div id="navbar"><p>navbar</p></div>
+ 
+ <div id="content">
+         <div class="article"><p>article</p>
+                 <div class="section"><p>section</p></div>
+         </div>
+    <div id="aside"><p>sidebar</p></div>
+     <div id="aside"><p>aside</p></div>
+ </div>
+ 
+ <div id="footer"><p>footer</p></div>
